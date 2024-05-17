4dd0a3efd3adb7b1829d7836223f478782c801bb
Mokeev Ivan <mig000085@gmai.com>
2024-05-17 15:36:01 +0400

    task2_3_4

---
+++ les2/task2.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+s1 = ((a + 1)//2)
+s2 = ((b + 1)//2)
+s3 = ((c + 1)//2)
+
+print(s1 + s2 + s3)
---
+++ les2/task3.py
@@ -0,0 +1,8 @@
+i = int(input())
+j = int(input())
+
+if i - j == 0:
+    print(-1)
+else:3
+
+print(i + j - 1)
