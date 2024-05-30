699c1edbbe81fb35506c482fe6c64f42874723ba
Ivan Mokeev <mig00085@gmail.com>
2024-05-30 15:12:09 +0400

    len3

---
+++ len3/mail.py
@@ -0,0 +1,11 @@
+# import modul1
+# print (modul1.max1(5, 9))
+
+# from modul1 import max1
+# print (max1(10, 9 ))
+
+# from modul1 import *
+# print (max1(10, 9 ))
+
+import modul1 as m1
+print (m1.max1(10, 9))
